9856ef6813b681d9445bf2d31dae2685e0571e0f
gariki <chetan.gariki@gmail.com>
2018-04-25 12:43:17 -0700

    kj

---
+++ colorChanger.js
@@ -0,0 +1,7 @@
+var myBtn = document.getElementsByTagName("button")[0];
+var myBody = document.getElementsByTagName("body")[0];
+
+myBtn.addEventListener("click", function (){
+    myBody.classList.toggle("colorbg");
+});
+
